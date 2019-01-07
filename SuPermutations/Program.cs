80c2fee0f2ace00c0b68c9a3a61e9e03482750b4
floatGOAT <pandarpr@yandex.ru>
2019-01-07 13:31:01 +0600

    Initial commit

---
+++ SuPermutations/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuPermutations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter n: ");
+            int n = Int32.Parse(Console.ReadLine());
+            Console.WriteLine($"Entered value = {n}");
+        }
+    }
+}
